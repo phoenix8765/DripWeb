710de7586594a8302982a310f982fd2b6e7689a6
Himanshu <miinu7788@gmail.com>
2025-05-19 16:06:44 +0530

    Title change in my home

--- src/component/MyHome.js
+++ src/component/MyHome.js
@@ -10,7 +10,7 @@ function MyHome() {
           NOW Available
         </p>
         <p className='description'>
-          NEW Drip series
+          NEW Himanshu's series
         </p>
       </div>
       <img className="product" src={Shirt1} alt="shirt1" />
